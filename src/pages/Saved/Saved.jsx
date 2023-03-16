7f81558798dc959ee2d445c08fcdf33107ffff86
Omar ESSOUFI <omar.essoufi@gmail.com>
2023-03-16 09:46:51 +0100

    finish Saved Page UI

--- src/pages/Saved/Saved.jsx
+++ src/pages/Saved/Saved.jsx
@@ -3,8 +3,13 @@ import settingIcon from "../../assets/images/setting.png";
 import SavedItemsIcon from "../../assets/images/savedItems.png";
 import FavorisIcon from "../../assets/images/favoris.png";
 import filterIcon from "../../assets/images/filter.png";
+import exampleImg from "../../assets/images/scenery.jpg";
+import profileImg from "../../assets/images/omar.jpg";
+import shareIcon from "../../assets/images/share2.png";
 import { Button } from "react-bootstrap";
 import { FiPlus } from "react-icons/fi";
+import { TbPoint } from "react-icons/tb";
+import { FiMoreHorizontal } from "react-icons/fi";
 
 const SideMenu = () => {
   return (
@@ -60,24 +65,103 @@ const SideMenu = () => {
   );
 };
 
+const PostSaved = () => {
+  return (
+    <div className="PostSaved">
+      <div
+        style={{
+          width: "130px",
+          height: "130px",
+          borderRadius: "8px",
+          overflow: "hidden",
+        }}
+      >
+        <img src={exampleImg} alt="" height={"100%"} />
+      </div>
+      <div className="w-100">
+        <h5 style={{ fontWeight: "700" }}>Bla Bla Text Blad</h5>
+
+        <p className="my-2" style={{ fontSize: "13px" }}>
+          <span style={{ color: "var(--color-light)" }}>
+            Post <TbPoint /> Saved to{" "}
+          </span>
+          Favoris
+        </p>
+
+        <div>
+          <img
+            src={profileImg}
+            alt=""
+            width={23}
+            style={{ borderRadius: "100%" }}
+          />
+          <span
+            style={{
+              fontSize: "13px",
+              color: "var(--color-light)",
+              marginLeft: "10px",
+            }}
+          >
+            Saved from{" "}
+          </span>
+          <span>CNN's Post</span>
+        </div>
+
+        <div
+          className="d-flex gap-2"
+          style={{ height: "40px", margin: "20px 10px 0 15px" }}
+        >
+          <div className="PostSavedBtns Center" style={{ maxWidth: "230px" }}>
+            Add to Collection
+          </div>
+          <div
+            className="PostSavedBtns Center"
+            style={{ fontSize: "20px", maxWidth: "50px" }}
+          >
+            <img
+              src={shareIcon}
+              alt=""
+              width={17}
+              style={{ filter: "invert(90%)" }}
+            />
+          </div>
+          <div
+            className="PostSavedBtns Center"
+            style={{ fontSize: "20px", maxWidth: "50px" }}
+          >
+            <FiMoreHorizontal />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const SubTitle = () => {
+  return (
+    <div className="SubTitlePage">
+      <h5>All</h5>
+      <div>
+        <div className="SavedFilter Center">
+          <img
+            src={filterIcon}
+            width={18}
+            alt=""
+            style={{ filter: "invert(90%)" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Saved = () => {
   return (
     <div className="Saved">
       <SideMenu />
-      <div className="SavedTitle">
-        <div className="SubTitlePage">
-          <h5>All</h5>
-          <div>
-            <div className="SavedFilter Center">
-              <img
-                src={filterIcon}
-                width={20}
-                alt=""
-                style={{ filter: "invert(90%)" }}
-              />
-            </div>
-          </div>
-        </div>
+      <div className="SavedPosts">
+        <SubTitle />
+        <PostSaved />
       </div>
     </div>
   );
